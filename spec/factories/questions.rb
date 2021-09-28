52a96ec6607cba31463d4e855b196cfc7c4a7d9b
aizhan-ayapbergenova <aizhan@robarin.pro>
2021-09-28 15:03:05 +0600

    Question controller specs

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :question do
-    title { "MyString" }
-    body { "MyText" }
+    title { "title sample" }
+    body { "text sample" }
+
+    factory :invalid_question do
+      title { nil }
+      body { nil }
+    end
   end
 end
